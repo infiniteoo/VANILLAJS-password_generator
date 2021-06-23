7ef03e7221a90a0effd57f1e93d202572b65dcfe
Troy Dorman <troydorman@gmail.com>
2021-06-23 07:29:05 -0600

    generated similar function for random uppercase character

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(getRandomLower());
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
