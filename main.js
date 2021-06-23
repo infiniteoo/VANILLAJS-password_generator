afec7596115eb74c12eee959bb9fe03420703e93
Troy Dorman <troydorman@gmail.com>
2021-06-23 07:30:04 -0600

    created function to generate a random number between 0 and 9

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ function getRandomLower() {
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber());
